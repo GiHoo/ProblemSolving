b8be6ca0dc9e11ff9a9aca0d0e45919d4e651b7d
kimnamgyu <lethargic.hood@gmail.com>
2023-10-21 16:26:05 +0900

    리트코드 : Group Anagramsˆ

---
+++ src/book/GroupAnagrams.java
@@ -0,0 +1,23 @@
+package book;
+
+import java.util.*;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String newStr = String.valueOf(chars);
+
+            if (!map.containsKey(newStr)) {
+                map.put(newStr, new ArrayList<>());
+            }
+            map.get(newStr).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
