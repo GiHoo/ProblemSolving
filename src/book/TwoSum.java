68c81e998fd9867246fc0b34973bb152c5d33dcb
kimnamgyu <lethargic.hood@gmail.com>
2023-10-22 19:43:13 +0900

    리트코드 : Two Sum

---
+++ src/book/TwoSum.java
@@ -0,0 +1,15 @@
+package book;
+
+public class TwoSum {
+    public int twoSum(int[] arr, int target) {
+        int answer = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            int num1 = arr[i];
+            for (int j = i+1; j < arr.length; j++) {
+                int num2 = arr[j];
+                if (target == (num1 + num2)) answer++;
+            }
+        }
+        return answer;
+    }
+}
