a95524a0d20dc1825190c89838b83d5edd4ba519
kimnamgyu <lethargic.hood@gmail.com>
2023-10-25 21:00:05 +0900

    백준

---
+++ src/boj/Main.java
@@ -0,0 +1,21 @@
+package boj;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = N; i > 0; i--) {
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
