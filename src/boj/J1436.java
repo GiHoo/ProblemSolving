9e69dd26b02ef4a6185084de0d64a3e7d34f17b0
kimnamgyu <lethargic.hood@gmail.com>
2024-01-15 23:22:10 +0900

    solve: 1436 영화감독 숀

---
+++ src/boj/J1436.java
@@ -0,0 +1,28 @@
+package boj;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class J1436 {
+
+    static int N;
+    static int start = 666;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        N = N - 1;
+
+        while (N > 0) {
+            if (start % 666 == 0) {
+
+            } else {
+
+            }
+        }
+
+        System.out.println(start);
+    }
+}
