5041962de2704b1caa41be466e310ae56f9bf934
kimnamgyu <lethargic.hood@gmail.com>
2024-06-18 18:55:34 +0900

    solve: 백준 9205 맥주 마시면서 걸어가기

--- src/boj/맥주_마시면서_걸어가기.java
+++ src/boj/맥주_마시면서_걸어가기.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class 맥주_마시면서_걸어가기 {
+
+    private static final String canArrive = "happy";
+    private static final String canNotArrive = "sad";
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -30,12 +34,48 @@ public class 맥주_마시면서_걸어가기 {
                 list.add(new Node(x, y));
             }
 
-            sb.append(canArriveFestival()).append("\n");
+            sb.append(canArriveFestival(list)).append("\n");
         }
+
+        System.out.println(sb);
     }
 
-    private static String canArriveFestival() {
-        return null;
+    private static String canArriveFestival(List<Node> list) {
+        int beer = 20;
+        boolean arrive = true;
+
+        Node end = list.get(list.size() - 1);
+        int endX = end.x;
+        int endY = end.y;
+
+        Node start = list.get(0);
+        list.remove(start);
+
+        int startX = start.x;
+        int startY = start.y;
+
+        while (!list.isEmpty()) {
+            Node next = list.get(0);
+            list.remove(next);
+
+            int nextX = next.x;
+            int nextY = next.y;
+
+            int distance = Math.abs(nextX - startX) + Math.abs(nextY - startY);
+            int move = Math.floorDiv(distance, 50);
+
+            System.out.println(distance + " " + move);
+
+            if (move > beer) {
+                arrive = false;
+                break;
+            }
+
+            startX = nextX;
+            startY = nextY;
+        }
+
+        return arrive ? canArrive : canNotArrive;
     }
 
     static class Node {
