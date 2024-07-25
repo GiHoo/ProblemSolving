3835374e3df6de68ae8c8e07b4376e22ddbbc2f1
kimnamgyu <lethargic.hood@gmail.com>
2024-07-25 23:04:37 +0900

    solve: 백준 바둑이포커

--- src/boj/봄버맨.java
+++ src/boj/봄버맨.java
@@ -1,8 +1,11 @@
 package boj;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class 봄버맨 {
 
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
