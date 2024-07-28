2a1a56fb1dc8dff94f645517af4c946ea48b7d84
kimnamgyu <lethargic.hood@gmail.com>
2024-07-28 23:11:03 +0900

    solve: 백준 봄버맨

--- src/boj/봄버맨.java
+++ src/boj/봄버맨.java
@@ -8,6 +8,6 @@ public class 봄버맨 {
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
+        System.out.println();
     }
 }
