246f6ce81741f0d4e05f96f33ddd3249acf8ca9a
kimnamgyu <lethargic.hood@gmail.com>
2024-08-14 22:46:27 +0900

    solve: 백준 보물섬

--- src/boj/보물섬.java
+++ src/boj/보물섬.java
@@ -3,11 +3,12 @@ package boj;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class 봄버맨 {
+public class 보물섬 {
 
     public static void main(String[] args) {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println();
+
     }
 }
+
